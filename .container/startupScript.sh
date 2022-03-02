1ce72944573a203ca978cc294672288a222dc86f
Bryan Ruiz <bryan@scarletbeast.com>
2022-03-02 18:57:38 -0500

    removed gitignore and readme from magento directory before copying over to /var/www

--- .container/startupScript.sh
+++ .container/startupScript.sh
@@ -2,16 +2,17 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
-    source ~/.zshrc
     cd /var/www
     git clone https://github.com/magento/magento2.git
-    cd magento
+    cd magento2
     git checkout tags/2.4.3-p1
     rm -rf .git
+    rm .gitignore
+    rm README.md
     cp -rf ./* ..
     cd ..
-    rm -rf magento
-    cp .container/env.php magento/app/etc/
+    rm -rf magento2
+    cp .container/env.php app/etc/
     cp .container/auth.json ./auth.json
 else
     echo "-- Not first container startup --"
