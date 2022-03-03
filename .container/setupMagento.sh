1e71423322a8ccc53c5507b64f067b74caf95f66
Bryan Ruiz <bryan@scarletbeast.com>
2022-03-02 19:31:53 -0500

    fixed order of magento setup script, also added reindexing

--- .container/setupMagento.sh
+++ .container/setupMagento.sh
@@ -2,7 +2,10 @@ cd /var/www
 php bin/magento module:enable --all
 magerun2 db:import -c gzip .container/bare.database.sql.gz
 magerun2 db:import -c gzip .container/sample.database.sql.gz
+php bin/magento module:disable Magento_Elasticsearch6 Magento_Elasticsearch7 Magento_Elasticsearch
 php bin/magento setup:install
+php bin/magento module:enable Magento_Elasticsearch6 Magento_Elasticsearch7 Magento_Elasticsearch
 php bin/magento setup:upgrade
-php bin/magento maintenance:disable
+php bin/magento index:reindex
 php bin/magento cache:clean && php bin/magento cache:flush
+php bin/magento maintenance:disable
